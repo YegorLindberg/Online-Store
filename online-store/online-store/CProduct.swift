46111d293f3608246f5219b0409980682ad8f6e1
YegorLindberg <mr.tsarap@mail.ru>
2018-08-17 20:52:48 +0300

    create and first changes

---
+++ online-store/online-store/CProduct.swift
@@ -0,0 +1,13 @@
+//
+//  CProduct.swift
+//  online-store
+//
+//  Created by Moore on 15.08.2018.
+//  Copyright © 2018 Moore. All rights reserved.
+//
+
+import UIKit
+
+class CProduct: NSObject {
+
+}
---
+++ online-store/online-store/ProductCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCollectionViewCell.swift
+//  online-store
+//
+//  Created by Moore on 15.08.2018.
+//  Copyright © 2018 Moore. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ImageViewProduct: UIImageView!
+    @IBOutlet weak var labelRating: UILabel!
+    @IBOutlet weak var labelNameProduct: UILabel!
+    @IBOutlet weak var labelPrice: UILabel!
+    
+}
